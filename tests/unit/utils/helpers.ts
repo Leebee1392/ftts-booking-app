2549d9cd1b1165f5454042c1f4a9e31b3b82ed82
Radu Giuclea <radu-alexandru.giuclea@kainos.com>
2022-11-23 17:10:38 +0000

    Added async/await to asynchronous validation functions in validate() method in unit/utils/helpers.js

--- tests/unit/utils/helpers.ts
+++ tests/unit/utils/helpers.ts
@@ -6,18 +6,19 @@ export interface RequestAndResponse {
     res: Response
 }
 
+// Attempt at manually chaining form validation middleware for a given request, response and validation schema
 export async function validate(req : Request, res: Response, schema : ValidatorSchema): Promise<RequestAndResponse> {
-    const validationFunctions = validateRequest(schema); // Returns 3 functions, in positions [0][0], [0][1], and [1]
-    const validationFunction1 = validationFunctions[0][0];
-    const validationFunction2 = validationFunctions[0][1];
+    const validationFunctions = validateRequest(schema); // Returns 3 functions (that express chains together in router methods to perform validation), in positions [0][0], [0][1], and [1]
+    const validationFunction1 = validationFunctions[0][0]; // async function
+    const validationFunction2 = validationFunctions[0][1]; // async function
     const validationFunction3 = validationFunctions[1];
 
     let finalReq: Request = req, finalRes: Response = res;
 
 
-    const nextFunction1 = (req: Request, res: Response, next: NextFunction) => {
+    const nextFunction1 = async (req: Request, res: Response, next: NextFunction) => {
         console.log("1: ", req, res) // Prints '1:  undefined undefined'
-        validationFunction2(req, res, nextFunction2);
+        await validationFunction2(req, res, nextFunction2);
     }
        
 
@@ -33,7 +34,7 @@ export async function validate(req : Request, res: Response, schema : ValidatorS
     }
 
     console.log("0: ", req, res) // Correctly prints contents of req and res
-    validationFunction1(req, res, nextFunction1); // Function has call to next
+    await validationFunction1(req, res, nextFunction1); // Function has call to next
 
     return { req: finalReq, res: finalRes };
 }
